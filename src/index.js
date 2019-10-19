e01ac51a281239cfa1d85961f4d4f5c9f6620234
Dmitry Svistun <dima1002010@live.com>
2019-10-20 01:39:55 +0300

    исправил первоначальную страницу, добавил prop-types

--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom';
 import App from "./components/app/App.jsx";
 
 const init = () => {
+  const settings = {
+    offers: [`Beautiful & luxurious apartment at great location`, `Wood and stone place`, `Canal View Prinsengracht`, `Nice, cozy, warm big bed apartament`]
+  };
+
   ReactDOM.render(
-      <App />,
+      <App
+        offers={settings.offers}
+      />,
       document.querySelector(`#root`)
   );
 };
